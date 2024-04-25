86fac80811273dcf07b8f1dfabe9bc2c2c2f8aaf
Adrian Paredes <adrian@emida.net>
2024-04-25 14:18:51 -0600

    Ejercicios no resueltos 1

---
+++ Ejercicios/no_resueltos/ejercicios_1.sql
@@ -0,0 +1,13 @@
+/*
+
+Utilizando SAKILA resuelve los siguientes ejercicios
+
+
+1.- Muestra el número de rentas totales en cada tienda
+2.- Muestra el monto total de los pagos recibidos en cada tienda (los pagos deben tener el formato $ monto MXN)
+3.- Necesitamos encontrar a los mejores clientes de la tienda, para ello debes listar el id, nombre, apellido, correo (todo en minusculas), tienda, dinero gastado y numero de rentas
+4.- Se requiere un reporte que muestre en una consulta el título y el total de ventas de las 5 películas mas vendidas ordenadas de mayor a menor 
+
+
+*/
+
