057544cb3e51df959ab788efa7a31982a222276e
Adrian Paredes <adrian@emida.net>
2024-04-25 14:24:15 -0600

    Agrego segunda tanda de ejercicios

---
+++ Ejercicios/no_resueltos/ejercicios_2.sql
@@ -0,0 +1,12 @@
+/*
+Utilizando SAKILA resuelve: 
+
+1.- Muestra la direccion completa de cada miembro del staff
+2.- Listar todas las peliculas rentadas por REGINALD KINDER y PEDRO CHESTNUT (se debe ver el titulo de la película y quien la rento) 
+3.- Muestra todos los actores que han participado en la película BUTCH PANTHER
+4.- Muestra una lista de los actores que han participado en películas de deportes
+5.- Mostrar una lista de las películas que ha alquilado el cliente que ha alquilado mas película
+6.- En una lista muestra las 3 ciudades con mayor numero de clientes
+7.- Muestra cual es la categoria con mas ventas en cada tienda
+8.- Muestra el idioma de las 5 películas que menos se rentan
+*/
